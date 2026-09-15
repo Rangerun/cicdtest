8417b414772564db340cb02ea0f98ffa2f8429f2
angel <773850083@qq.com>
2026-09-15 08:13:53 +0800

    test

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -9,7 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, World!")
 	})
-
 	fmt.Println("Starting server on :8080...")
 	http.ListenAndServe(":8080", nil)
 }
